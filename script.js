9704c10b1764d1b1d1043882a4c6a1a5f7aaf90c
Imani muthoni <imanimuthoni9@gmail.com>
2026-05-28 12:18:33 +0300

    commented my work

--- script.js
+++ script.js
@@ -6,23 +6,36 @@ const textarea = document.getElementById("textarea")
 const displayNotes = document.getElementById("display-notes")
 // save btn
 saveBtn.addEventListener("click", (event)=>{
+    // stoped browser default
     event.preventDefault()
+    // created new elements to hold the textareas value
     const div = document.createElement("div")
     const p = document.createElement("p")
+    // kept the note in a p tag 
     p.textContent = textarea.value
+    // appended the p tag to the div so it can have a lovely background color 😘
     div.append(p)
+    // gave the div a class so it can look beautiful 💞
     div.classList.add("div")
+    //  gave the p a class so it can look beautiful 😍
     p.classList.add("saved-text")
-    const delete_btn = document.createElement("button")   
+    // working on delete btn ⏱️
+    const delete_btn = document.createElement("button")
+    // made the note visible on the body👁️   
     displayNotes.appendChild(div)
+    // after saving the textarea will be empty🫥 
     textarea.value = ""
+    // stopped displayind empty notes🛑
     if(p.textContent === ""){
        displayNotes.removeChild(div) 
     }
+
 })
+// event listener for clear btn
 clearBtn.addEventListener("click", () => {
     displayNotes.innerHTML = ""
 })
+// event listener for new btn
 newBtn.addEventListener("click", ()=>{
      textarea.value = ""
 })
